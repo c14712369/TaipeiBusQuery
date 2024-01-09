7b842268ffec1780747e28602b1b746c5cacfad8
yuhan <c14712369@gmail.com>
2024-01-10 01:38:24 +0800

    20240410

--- js/index.js
+++ js/index.js
@@ -1,19 +1,9 @@
-function searchBus() {
-    // 在此處實現搜尋公車資訊的邏輯
-    // 可以使用 JavaScript 來動態生成和更新列表
-    // 也可以使用 AJAX 或其他技術從後端獲取資料
-}
+function onTitleLoad() {
+    var squareElements = document.querySelectorAll('.square');
 
-function showRoute(busNumber) {
-    // 在此處實現顯示路線上所有站名的邏輯
-    // 可以使用 JavaScript 來動態生成和更新列表
-    // 也可以使用 AJAX 或其他技術從後端獲取資料
-}
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-} else {
-    console.log("此瀏覽器無法使用navigator.geolocation");
+    squareElements.forEach(function (square) {
+        square.classList.remove('square-hidden');
+    });
 }
 
 function successCallback(position) {
@@ -25,3 +15,24 @@ function successCallback(position) {
 function errorCallback(error) {
     console.error("無法取得User地理位置： " + error.message);
 }
+
+function redirect(url) {
+    window.location.href = url;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var titleElement = document.getElementById('title');
+    titleElement.addEventListener('animationend', function (event) {
+        setTimeout(() => {
+            if (event.animationName === 'moveUp') {
+                onTitleLoad();
+
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+                } else {
+                    console.log("此瀏覽器無法使用navigator.geolocation");
+                }
+            }
+        }, 300)
+    });
+});
