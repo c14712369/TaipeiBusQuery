0b5db485c3e3a47726363bdc52a4fd955c626480
c14712369 <c14712369@gmail.com>
2024-01-20 17:28:02 +0800

    20240120

--- js/routeQuery.js
+++ js/routeQuery.js
@@ -1,160 +1,175 @@
-const input = document.getElementById('busSearch');
-const resultsContainer = document.getElementById('results-container');
+const input = document.getElementById("busSearch");
+const resultsContainer = document.getElementById("results-container");
 
 let TaipeiBusRoutes = [];
 let NewTaipeiBusRoutes = [];
 let AllBusRoutes = [];
 
-let TaipeiRes = '';
-let NewTaipeiRes = '';
+let TaipeiRes = "";
+let NewTaipeiRes = "";
 
-let token = '';
-let response = '';
+let token = "";
+let response = "";
 
 document.addEventListener("DOMContentLoaded", function () {
-    getAuthorizationHeader();
+  getAuthorizationHeader();
 });
 
 function getAuthorizationHeader() {
-    const parameters = {
-        grant_type: "client_credentials",
-        client_id: "c14712369-dfd8e505-1843-44f2",
-        client_secret: "a6900643-59bf-41a3-b142-d3b4bc99c575"
-    };
+  const parameters = {
+    grant_type: "client_credentials",
+    client_id: "c14712369-dfd8e505-1843-44f2",
+    client_secret: "a6900643-59bf-41a3-b142-d3b4bc99c575",
+  };
 
-    const authUrl = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
+  const authUrl =
+    "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
 
-    fetch(authUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams(parameters),
+  fetch(authUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(parameters),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      token = data.access_token;
+      getApiResponse();
     })
-        .then(response => response.json())
-        .then(data => {
-            token = data.access_token;
-            // 請在這裡調用 API 請求
-            getApiResponse();
-        })
-        .catch(error => {
-            // 處理錯誤
-            console.error('Error:', error);
-        });
+    .catch((error) => {
+      console.log("Error:", error);
+    });
 }
 
 function getApiResponse() {
-    if (token) {
-        const taipeiApiUrl = 'https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/Taipei?$format=JSON';
-        const newTaipeiApiUrl = 'https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/NewTaipei?$format=JSON';
+  if (token) {
+    const taipeiApiUrl =
+      "https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/Taipei?$format=JSON";
+    const newTaipeiApiUrl =
+      "https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/NewTaipei?$format=JSON";
 
-        // 發送第一個 API 請求
-        fetch(taipeiApiUrl, {
-            method: 'GET',
-            headers: {
-                "Authorization": "Bearer " + token,
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                TaipeiRes = data;
-                TaipeiBusRoutes = data.map((i) => {
-                    return i.RouteName.Zh_tw;
-                });
-                AllBusRoutes = AllBusRoutes.concat(TaipeiBusRoutes);
-            })
-            .catch(error => {
-                // 處理 API 請求錯誤
-                console.error('Error:', error);
-            });
+    // 發送第一個 API 請求
+    fetch(taipeiApiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        TaipeiRes = data;
+        TaipeiBusRoutes = data.map((i) => {
+          return {
+            RouteName: i.RouteName.Zh_tw,
+            City: i.City,
+          };
+        });
+        AllBusRoutes = AllBusRoutes.concat(TaipeiBusRoutes);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
 
-        // 發送第二個 API 請求
-        fetch(newTaipeiApiUrl, {
-            method: 'GET',
-            headers: {
-                "Authorization": "Bearer " + token,
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                NewTaipeiRes = data;
-                NewTaipeiBusRoutes = data.map((i) => {
-                    return i.RouteName.Zh_tw;
-                });
-                AllBusRoutes = AllBusRoutes.concat(NewTaipeiBusRoutes);
-            })
-            .catch(error => {
-                // 處理 API 請求錯誤
-                console.error('Error:', error);
-            });
-    }
+    // 發送第二個 API 請求
+    fetch(newTaipeiApiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        NewTaipeiRes = data;
+        NewTaipeiBusRoutes = data.map((i) => {
+          return {
+            RouteName: i.RouteName.Zh_tw,
+            City: i.City,
+          };
+        });
+        AllBusRoutes = AllBusRoutes.concat(NewTaipeiBusRoutes);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }
 }
 
-input.addEventListener('input', function () {
-    const inputValue = input.value.trim().toLowerCase();
-    const resultItemClass = 'result-item';
-    const matchingRoutes = AllBusRoutes.filter(route => route.slice(0, inputValue.length).toLowerCase() === inputValue);
-    resultsContainer.innerHTML = '';
+input.addEventListener("input", function () {
+  const inputValue = input.value.trim().toLowerCase();
+  const resultItemClass = "result-item";
+  const matchingRoutes = AllBusRoutes.filter((route) =>
+    route.RouteName.toLowerCase().startsWith(inputValue)
+  );
+  let matchingRouteCity = matchingRoutes[0].City;
+  resultsContainer.innerHTML = "";
 
-    if (inputValue !== '') {
-        if (matchingRoutes.length > 0) {
-            matchingRoutes.forEach(route => {
-                const resultItem = document.createElement('div');
-                resultItem.classList.add(resultItemClass);
+  if (inputValue !== "") {
+    if (matchingRoutes.length > 0) {
+      matchingRoutes.forEach((route) => {
+        const resultItem = document.createElement("div");
+        resultItem.classList.add(resultItemClass);
 
-                // 找到對應的路線資訊
-                const routeInfo = findRouteInfo(route);
+        // 找到對應的路線資訊
+        const routeInfo = findRouteInfo(route.RouteName);
 
-                if (routeInfo) {
-                    // 建立顯示資訊的元素
-                    const routeInfoElement = document.createElement('div');
-                    routeInfoElement.innerHTML = `<span>${routeInfo.DepartureStopNameZh} — ${routeInfo.DestinationStopNameZh}</span><br><p>路線名稱： ${route}</p>`;
+        if (routeInfo) {
+          // 建立顯示資訊的元素
+          const routeInfoElement = document.createElement("div");
+          routeInfoElement.innerHTML = `<span>${routeInfo.DepartureStopNameZh} — ${routeInfo.DestinationStopNameZh}</span><br><p>路線名稱： ${route.RouteName}</p>`;
 
-                    // 添加ID
-                    routeInfoElement.setAttribute('id', route);
+          // 添加ID
+          routeInfoElement.setAttribute("id", route);
 
-                    // 將資訊加入 resultItem
-                    resultItem.appendChild(routeInfoElement);
+          // 將資訊加入 resultItem
+          resultItem.appendChild(routeInfoElement);
 
-                    // 傳送路線名稱
-                    sessionStorage.setItem('DestinationStop', routeInfo.DestinationStopNameZh);
-                    sessionStorage.setItem('DepartureStop', routeInfo.DepartureStopNameZh);
-                    sessionStorage.setItem('Route', route);
-                } else {
-                    // 若找不到路線資訊，僅顯示路線名稱
-                    resultItem.textContent = route;
-                }
-                // 將 resultItem 加入到 resultsContainer
-                resultsContainer.appendChild(resultItem);
+          // 傳送路線名稱
+          sessionStorage.setItem(
+            "DestinationStop",
+            routeInfo.DestinationStopNameZh
+          );
+          sessionStorage.setItem(
+            "DepartureStop",
+            routeInfo.DepartureStopNameZh
+          );
+          sessionStorage.setItem("RouteName", route.RouteName);
+        } else {
+          // 若找不到路線資訊，僅顯示路線名稱
+          resultItem.textContent = route;
+        }
+        // 將 resultItem 加入到 resultsContainer
+        resultsContainer.appendChild(resultItem);
 
-                // 設定點擊事件
-                resultItem.addEventListener('click', function () {
-                    let stopApiUrl = `https://tdx.transportdata.tw/api/basic/v2/Bus/StopOfRoute/City/Taipei/${route}?$top=30&$format=JSON`;
-                    fetch(stopApiUrl, {
-                        method: 'GET',
-                        headers: {
-                            "Authorization": "Bearer " + token,
-                        },
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            sessionStorage.setItem('RouteData', JSON.stringify(data));
-                            window.location.href = '../html/showQuery.html';
-                        })
-                        .catch(error => {
-                            // 處理 API 請求錯誤
-                            console.error('Error:', error);
-                        });
-                });
+        // 設定點擊事件
+        resultItem.addEventListener("click", function () {
+          let stopApiUrl = `https://tdx.transportdata.tw/api/basic/v2/Bus/StopOfRoute/City/${route.City}/${route.RouteName}?$top=30&$format=JSON`;
+          fetch(stopApiUrl, {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              sessionStorage.setItem("RouteData", JSON.stringify(data));
+              sessionStorage.setItem("RouteCity", matchingRouteCity);
+              window.location.href = "../html/showQuery.html";
+            })
+            .catch((error) => {
+              console.log("Error:", error);
             });
-        };
+        });
+      });
     }
-})
+  }
+});
 
 // 在所有路線中尋找特定路線的資訊
 function findRouteInfo(routeName) {
-    const allRoutes = TaipeiRes.concat(NewTaipeiRes);
-    const routeInfo = allRoutes.find(route => route.RouteName.Zh_tw === routeName);
-    return routeInfo;
+  const allRoutes = TaipeiRes.concat(NewTaipeiRes);
+  const routeInfo = allRoutes.find(
+    (route) => route.RouteName.Zh_tw === routeName
+  );
+  return routeInfo;
 }
-
--- js/showQuery.js
+++ js/showQuery.js
@@ -1,134 +1,162 @@
-let receivedData = sessionStorage.getItem('RouteData');
-let route = sessionStorage.getItem('Route');
-let DestinationStop = sessionStorage.getItem('DestinationStop');
-let DepartureStop = sessionStorage.getItem('DepartureStop');
+let receivedData = sessionStorage.getItem("RouteData");
+let route = sessionStorage.getItem("RouteName");
+let RouteCity = sessionStorage.getItem("RouteCity");
+let DestinationStop = sessionStorage.getItem("DestinationStop");
+let DepartureStop = sessionStorage.getItem("DepartureStop");
 let RouteRealTimeData;
-let apiID = 'c14712369-dfd8e505-1843-44f2';
-let apiKey = 'a6900643-59bf-41a3-b142-d3b4bc99c575';
+let apiID = "c14712369-dfd8e505-1843-44f2";
+let apiKey = "a6900643-59bf-41a3-b142-d3b4bc99c575";
 let options = {
-    "method": "post",
-    "headers": {
-        "content-type": "application/x-www-form-urlencoded"
-    },
-    "payload": {
-        "grant_type": "client_credentials",
-        "client_id": apiID,
-        "client_secret": apiKey
-    }
+  method: "post",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded",
+  },
+  payload: {
+    grant_type: "client_credentials",
+    client_id: apiID,
+    client_secret: apiKey,
+  },
 };
 
 if (receivedData) {
-    let parsedData = JSON.parse(receivedData);
-    // 把去、回程資料各自抓出來
-    let to = parsedData[0].Stops;
-    let from = parsedData[1].Stops;
+  let parsedData = JSON.parse(receivedData);
 
-    // 去程
-    const toRoute = document.querySelector('.toRoute');
-    const toTitle = document.createElement('div');
-    toTitle.className = 'Title';
-    toTitle.innerHTML = `<p>往${DestinationStop}</p>`;
-    toRoute.appendChild(toTitle);
+  // 把去、回程資料各自抓出來
+  let to = parsedData[0].Stops;
+  let from = parsedData[1].Stops;
 
-    for (let i = 0; i < to.length; i++) {
+  // 去程
+  const toRoute = document.querySelector(".toRoute");
+  const toTitle = document.createElement("div");
+  toTitle.className = "Title";
+  toTitle.innerHTML = `<p>往${DestinationStop}</p>`;
+  toRoute.appendChild(toTitle);
 
-        if (i < to.length - 1) {
-            const busStop = document.createElement('div');
-            busStop.className = 'bus-stopCon';
-            busStop.innerHTML = `<div class="bus-stop"><div class="line"></div></div><p>${to[i].StopName.Zh_tw}</p>`;
-            toRoute.appendChild(busStop);
-        } else {
-            const busStop = document.createElement('div');
-            busStop.className = 'bus-stopCon';
-            busStop.innerHTML = `<div class="bus-stop"><div></div></div><p>${to[i].StopName.Zh_tw}</p>`;
-            toRoute.appendChild(busStop);
-        }
+  // 抓取當前每台路線公車的座標
+  getAuthorizationHeader();
+  console.log(RouteRealTimeData);
+
+  for (let i = 0; i < to.length; i++) {
+    //算出每個站牌，距離每個當前路線公車的時間，找出最短的那台並顯示到站時間
+    let stopCoords = [
+      to[i].StopPosition.PositionLat,
+      to[i].StopPosition.PositionLon,
+    ];
+
+    initMap(stopCoords, RouteRealTimeData);
+
+    // 處理HTML
+    if (i < to.length - 1) {
+      const busStop = document.createElement("div");
+      busStop.className = "bus-stopCon";
+      busStop.innerHTML = `<div class="bus-stop"><div class="line"></div></div><p>${to[i].StopName.Zh_tw}</p><button>123</button>`;
+      toRoute.appendChild(busStop);
+    } else {
+      const busStop = document.createElement("div");
+      busStop.className = "bus-stopCon";
+      busStop.innerHTML = `<div class="bus-stop"><div></div></div><p>${to[i].StopName.Zh_tw}</p><button>123</button>`;
+      toRoute.appendChild(busStop);
     }
+  }
 
-    // 回程
-    const fromRoute = document.querySelector('.fromRoute');
-    const fromTitle = document.createElement('div');
-    fromTitle.className = 'Title';
-    fromTitle.innerHTML = `<p>往${DepartureStop}</p>`;
-    fromRoute.appendChild(fromTitle);
+  // 回程
+  const fromRoute = document.querySelector(".fromRoute");
+  const fromTitle = document.createElement("div");
+  fromTitle.className = "Title";
+  fromTitle.innerHTML = `<p>往${DepartureStop}</p>`;
+  fromRoute.appendChild(fromTitle);
 
-    for (let i = 0; i < from.length; i++) {
-
-        if (i < from.length - 1) {
-            const busStop = document.createElement('div');
-            busStop.className = 'bus-stopCon';
-            busStop.innerHTML = `<div class="bus-stop"><div class="line"></div></div><p>${from[i].StopName.Zh_tw}</p>`;
-            fromRoute.appendChild(busStop);
-        } else {
-            const busStop = document.createElement('div');
-            busStop.className = 'bus-stopCon';
-            busStop.innerHTML = `<div class="bus-stop"><div></div></div><p>${from[i].StopName.Zh_tw}</p>`;
-            fromRoute.appendChild(busStop);
-        }
+  for (let i = 0; i < from.length; i++) {
+    if (i < from.length - 1) {
+      const busStop = document.createElement("div");
+      busStop.className = "bus-stopCon";
+      busStop.innerHTML = `<div class="bus-stop"><div class="line"></div></div><p>${from[i].StopName.Zh_tw}</p><button>123</button>`;
+      fromRoute.appendChild(busStop);
+    } else {
+      const busStop = document.createElement("div");
+      busStop.className = "bus-stopCon";
+      busStop.innerHTML = `<div class="bus-stop"><div></div></div><p>${from[i].StopName.Zh_tw}</p><button>123</button>`;
+      fromRoute.appendChild(busStop);
     }
-
+  }
 } else {
-    console.log('查無資料！');
+  console.log("查無資料！");
 }
 
-if (route) {
+function getAuthorizationHeader() {
+  const parameters = {
+    grant_type: "client_credentials",
+    client_id: "c14712369-dfd8e505-1843-44f2",
+    client_secret: "a6900643-59bf-41a3-b142-d3b4bc99c575",
+  };
 
-    document.addEventListener("DOMContentLoaded", function () {
-        getAuthorizationHeader();
+  const authUrl =
+    "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
+
+  fetch(authUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(parameters),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      token = data.access_token;
+      getApiResponse();
+    })
+    .catch((error) => {
+      console.log("Error:", error);
     });
+}
 
-    function getAuthorizationHeader() {
-        const parameters = {
-            grant_type: "client_credentials",
-            client_id: "c14712369-dfd8e505-1843-44f2",
-            client_secret: "a6900643-59bf-41a3-b142-d3b4bc99c575"
-        };
+function getApiResponse() {
+  if (token) {
+    const realTimeApiUrl = `https://tdx.transportdata.tw/api/basic/v2/Bus/RealTimeByFrequency/City/${RouteCity}/${route}?%24top=30&%24format=JSON`;
 
-        const authUrl = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
+    // 發送第一個 API 請求
+    fetch(realTimeApiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        RouteRealTimeData = data.map((item) => ({
+          Lat: item.BusPosition.PositionLat,
+          Lon: item.BusPosition.PositionLon,
+        }));
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }
+}
 
-        fetch(authUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams(parameters),
-        })
-            .then(response => response.json())
-            .then(data => {
-                token = data.access_token;
-                // 請在這裡調用 API 請求
-                getApiResponse();
-            })
-            .catch(error => {
-                // 處理錯誤
-                console.error('Error:', error);
-            });
+function initMap(startCoords, RouteRealTimeData) {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: startCoords[0], lng: startCoords[1] },
+    zoom: 14,
+  });
+
+  directionsRenderer.setMap(map);
+
+  var request = {
+    origin: { lat: startCoords[0], lng: startCoords[1] },
+    destination: { lat: RouteRealTimeData[0], lng: RouteRealTimeData[1] },
+    travelMode: "DRIVING",
+  };
+
+  directionsService.route(request, function (result, status) {
+    if (status == "OK") {
+      directionsRenderer.setDirections(result);
+      var drivingTime = result.routes[0].legs[0].duration.text;
+      console.log("大約抵達時間：", drivingTime);
+    } else {
+      console.error("搜尋失敗：", status);
     }
-
-    function getApiResponse() {
-        if (token) {
-            const realTimeApiUrl = `https://tdx.transportdata.tw/api/basic/v2/Bus/RealTimeByFrequency/City/Taipei/${route}?%24top=30&%24format=JSON`;
-
-            // 發送第一個 API 請求
-            fetch(realTimeApiUrl, {
-                method: 'GET',
-                headers: {
-                    "Authorization": "Bearer " + token,
-                },
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    const RouteRealTimeData = data.map((item) => ({
-                        Azimuth: item.Azimuth,
-                        // 其他需要的屬性
-                    }));
-                    console.log(RouteRealTimeData);
-                })
-                .catch(error => {
-                    // 處理 API 請求錯誤
-                    console.error('Error:', error);
-                });
-        }
-    }
+  });
 }
